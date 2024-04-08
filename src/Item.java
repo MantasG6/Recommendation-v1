8e3c777a0219cf0624197094889a16a5688a65d0
Mantas Gricius <mantas.gricius6@gmail.com>
2024-04-08 11:35:10 +0300

    Initial commit

---
+++ src/Item.java
@@ -0,0 +1,22 @@
+public class Item {
+
+    private String itemName;
+
+    public Item(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemName='" + itemName + '\'' +
+                '}';
+    }
+}
